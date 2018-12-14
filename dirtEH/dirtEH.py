11fe24418f18d5486b74e3024ea7ead47dc8d840
Joe Gallagher <joedgallagher@yahoo.com>
2018-12-14 00:07:16 -0500

    Basic file format and directory read

--- dirtEH/dirtEH.py
+++ dirtEH/dirtEH.py
@@ -1,1 +1,12 @@
+import numpy as np
+import os
+
+
+def getPaths(dir):
+    files = os.listdir(dir)
+    for i in files:
+        print(dir+i)
+
+
 print("Hello, Canada?")
+getPaths("C:/Users/Owner/Documents/Portfolio/Media/Collage Canidates/")
